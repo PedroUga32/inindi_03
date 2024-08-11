3d3cb25be93a0f63208e518a2a7675295ff42681
PedroUga32 <115152166+PedroUga32@users.noreply.github.com>
2024-08-11 14:49:51 -0300

    ok

--- src/test.cpp
+++ src/test.cpp
@@ -7,14 +7,27 @@
 void setup()
 {
     InIndKit.setup("Wokwi-GUEST", "", "inindkit10");
-    #define amostras 100
-    #define amplitude 1.0
+    #define amostras 200
+    #define amplitude 2
     
     double ondatri[amostras];
     int i;
     double passo = (2*amplitude)/(amostras/2);
-    
 
+    for (i = 0; i < (amostras / 2); i++) {
+        ondatri[i] = passo * i;
+    }
+
+    for (i = (amostras / 2); i < amostras; i++) {
+        ondatri[i] = 2 * amplitude - passo * (i - amostras / 2);
+    }
+
+    // Exibindo a forma de onda
+    for (i = 0; i < amostras; i++) {
+        printf("%f\n", ondatri[i]);
+    }
+
+    return 0;
 }
 
 void loop()
