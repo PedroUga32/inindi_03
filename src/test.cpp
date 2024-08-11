eb54f47a2b3c344b7d274702d2aa3a99eaf33e43
PedroUga32 <115152166+PedroUga32@users.noreply.github.com>
2024-08-11 14:40:25 -0300

    ok

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,28 @@
 #include <Arduino.h>
 #include "InIndKit.h"
+#include <stdio.h>
+#include <math.h>
+
 
 void setup()
 {
     InIndKit.setup("Wokwi-GUEST", "", "inindkit10");
+    #define amostras 100
+    #define amplitude 1.0
+    
+    double ondatri[amostras];
+    int i;
+    double passo = (2*amplitude)/(amostras/2);
+    
+
 }
 
 void loop()
 {
     InIndKit.loop();
+
+
+
+
+
 }
